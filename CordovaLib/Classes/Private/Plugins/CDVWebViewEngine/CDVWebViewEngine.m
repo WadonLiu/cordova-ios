89390a413f8065643a264681d214bb43f9ce753c
WadonLiu <812002035@qq.com>
2023-12-12 11:42:44 +0800

    Update CDVWebViewEngine.m

--- CordovaLib/Classes/Private/Plugins/CDVWebViewEngine/CDVWebViewEngine.m
+++ CordovaLib/Classes/Private/Plugins/CDVWebViewEngine/CDVWebViewEngine.m
@@ -243,9 +243,11 @@
         wkWebView.navigationDelegate = (id <WKNavigationDelegate>)self;
     }
 
+    /*
     if ([self.viewController conformsToProtocol:@protocol(WKScriptMessageHandler)]) {
         [wkWebView.configuration.userContentController addScriptMessageHandler:(id < WKScriptMessageHandler >)self.viewController name:CDV_BRIDGE_NAME];
     }
+    */
 
     [self updateSettings:settings];
 
